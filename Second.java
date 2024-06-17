f980b9e503b1c887c8e74ec116b125cd79652596
ilayarajac515 <ilayarajac515@gmail.com>
2024-06-18 01:54:16 +0530

    new changes

--- Second.java
+++ Second.java
@@ -2,5 +2,7 @@ class Second{
       public static void main(String q[])
       {
           System.out.println("Checking connect with github and jenkins");
+
+          System.out.println("checing for new update");
       }
 }

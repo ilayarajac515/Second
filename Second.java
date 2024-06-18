713d5ff242d97a2e10458262aace1152cb4c0db3
ilayarajac515 <ilayarajac515@gmail.com>
2024-06-18 15:26:52 +0530

    delete work

--- Second.java
+++ Second.java
@@ -2,9 +2,5 @@ class Second{
       public static void main(String q[])
       {
           System.out.println("Checking connect with github and jenkins");
-
-          System.out.println("checing for new update");
-
-          System.out.println("another check");
       }
 }

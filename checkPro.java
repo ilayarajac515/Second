9998b63587ea28faa6c4e0e2fed0c1c7930f8eac
ilayarajac515 <ilayarajac515@gmail.com>
2024-06-19 14:33:41 +0530

    Update checkPro.java

--- checkPro.java
+++ checkPro.java
@@ -1,6 +1,6 @@
 class checkPro{
       public static void main(String q[])
       {
-          System.out.println("Checking connect with github and jenkins");
+          System.out.println("Third Job");
       }
 }

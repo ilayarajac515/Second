25b4306dfc57bf40a24b46f2b617034ef09d0b93
ilayarajac515 <ilayarajac515@gmail.com>
2024-07-07 18:36:20 +0530

    change

--- helloWorld.java
+++ helloWorld.java
@@ -1,10 +1,5 @@
 class helloWorld{
     public static void main(String args[]){
         System.out.println("hello world");
-        System.out.println("hi");
-        System.out.println("hello");
-        System.out.println("how are you");
-        System.out.println("fine ");
-        System.out.println("i hope doing good well");
     }
 }
